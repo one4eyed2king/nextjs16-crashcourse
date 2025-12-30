4edd46f7039536a55579b54c2d71452795410fb1
holdenzerega <128439745+holdenzerega@users.noreply.github.com>
2025-12-30 16:05:17 -0500

    implelemt posthog

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,12 +1,18 @@
 'use client';
 
 import Image from "next/image";
+import posthog from "posthog-js";
 
 const ExploreBtn = () => {
+    const handleClick = () => {
+        console.log('clicked');
+        posthog.capture('explore_events_clicked', {
+            button_location: 'homepage_hero',
+        });
+    };
+
     return (
-        <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => {
-            console.log('clicked')
-        }}>
+        <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={handleClick}>
             <a href="#events">
                 Explore Events
                 <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24}/>
